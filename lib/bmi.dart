3e9f1f846e3a08e83fa7e650b97de89a27ab0d11
madedeva <deva.kerti@undiksha.ac.id>
2022-09-26 22:36:45 +0800

    first commit

---
+++ lib/bmi.dart
@@ -0,0 +1,128 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'bmi_result.dart';
+
+class InputBMI extends StatefulWidget {
+  const InputBMI({Key? key}) : super(key: key);
+
+  @override
+  _InputBMIState createState() => _InputBMIState();
+}
+
+class _InputBMIState extends State<InputBMI> {
+  int tinggi = 0;
+  int berat = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        //backgroundColor: Colors.blue[900],
+        centerTitle: true,
+        leading: const Icon(
+          Icons.favorite,
+          color: Colors.pink,
+        ),
+        title: const Text('MENGHITUNG BMI'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Image.asset(
+              'image/bmi.png',
+              fit: BoxFit.fitWidth,
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.all(10),
+                // color: Colors.blue[700],
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        onChanged: (txt) {
+                          setState(() {
+                            tinggi = int.parse(txt);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        maxLength: 3,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                        decoration: const InputDecoration(
+                            suffix: Text('cm'),
+                            filled: true,
+                            hintText: 'Tinggi'),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        onChanged: (txt) {
+                          setState(() {
+                            berat = int.parse(txt);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        maxLength: 3,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                        decoration: const InputDecoration(
+                            suffix: Text('kg'),
+                            filled: true,
+                            hintText: 'Berat'),
+                      ),
+                    ),
+                  ],
+                )),
+            Container(
+              //height: double.infinity,
+              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => BMIResult(
+                            tinggi_badan: tinggi, berat_badan: berat)),
+                  );
+                },
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                color: Colors.black,
+                // textColor: Colors.white,
+                child: const Text(
+                  'HITUNG BMI',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        //color: Colors.transparent,
+        child: Container(
+          height: 60,
+          color: Colors.black54,
+          alignment: Alignment.center,
+          child: const Text(
+            'Developed by Resika Arthana',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white),
+          ),
+        ),
+        //elevation: 0,
+      ),
+    );
+  }
+}
